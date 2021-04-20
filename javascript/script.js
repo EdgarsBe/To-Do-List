b3182468a7b9ab58ae838c13e0933b847c8a71a5
EdgarsBe <edgarsberzins100@gmail.com>
2021-04-20 14:54:36 +0300

    buttons js

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+const POP_UP = document.getElementById('Pop_up');
+let Uzdevumi = [];
+
+document.getElementById('Jauns_uzd').addEventListener('click', () => {
+    POP_UP.style.display = 'block';
+})
+document.getElementById('Pievienot_uzd').addEventListener('click', () => {
+    POP_UP.style.display = 'none'
+})
+
